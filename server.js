876866c1f212c67f94d714d053b87f969492ce79
unknown <andreyandreydn16@gmail.com>
2024-03-03 13:05:04 +0300

    add cors lib

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
-const express = require('express');
+const express = require("express");
+const cors = require("cors");
 const app = express();
 const PORT = 8080;
 
 app.use(express.json());
+app.use(cors());
 
-app.post('/api/button-click', (req, res) => {
+app.post("/api/button-click", (req, res) => {
   const { buttonId } = req.body;
   const flag = buttonId === 2 ? false : true;
   res.json({ flag });
